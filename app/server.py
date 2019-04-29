b4ce70a961233052416f5dbb7cb03fc563f82207
Sophia Lau <sophialau52699@gmail.com>
2019-04-29 13:47:28 -0700

    new flowers model

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from fastai import *
 from fastai.vision import *
 
 # export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
-export_file_url = 'https://drive.google.com/uc?export=download&id=1e-i3x9l_HMDfpmKAe_1n5TOXDbHvbM9q'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1MivW1WQxKvgdHyvK2_rEhnR53oe6x_8v'
 export_file_name = 'flowers_resnext101_1.pth'
 
 classes = ['rose', 'daisy', 'dandelion', 'tulip', 'sunflower']
